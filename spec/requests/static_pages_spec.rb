01efc9edb92342c355b64b1ff4c94a58ee6d8e9b
Georg Dresler <georg@g-dresler.de>
2012-09-06 21:43:43 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,11 +4,62 @@ describe "StaticPages" do
   
   describe "home page" do
     
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
 
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby Tut Sample App | Home")
     end
   end
+
+  describe "help page" do
+    
+    it "should have the h1 'Help'" do
+
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Ruby Tut Sample App | Help")
+    end
+  end
+
+  describe "about page" do
+    
+    it "should have the h1 'About Us" do
+
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby Tut Sample App | About Us")
+    end
+  end
+
+  describe "contact page" do
+    
+    it "should have the h1 'Contact" do
+
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Ruby Tut Sample App | Contact")
+    end
+  end  
 
 end
